c00c1762b3f025f0fbcf2bf4454732e81bc3b8ae
Aakif usmani <aakifusmanismas@gmail.com>
2021-10-04 22:49:32 +0500

    very hard but i can do it

--- src/expenses/Expenses.js
+++ src/expenses/Expenses.js
@@ -1,33 +1,88 @@
-import React from "react";
-import './Expenses.css'
+import React, { useState } from "react";
+import "./Expenses.css";
 
-
-const Card = (props) => {
-    const classes = 'card ' + props.className;
-    return <div className={classes}>{props.children}</div>;
-};
 const Expenses = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  const [showHide, setShowHide] = useState(false);
 
-    
-    return(
-       
-        <div className="Card" >
-            <h1>lets start!</h1>
-     <div>
-       input div
-     </div>
-     <div>
-       chart div
-     </div>
-     <div>
-       filter div
-     </div>
-     <div>
-       expense list div
-     </div>
-        </div>
-      
-    )
+  const showHideForm = () => {
+    setShowHide(!showHide);
+  };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  setEnteredTitle("");
+  setEnteredDate("");
+  setEnteredAmount("");
+ 
+  return (
+    <div className="main">
+      <h1> EXPENSE MANAGER</h1>
+      <div className="form-input">
+        {/* input div */}
+        <form>
+          {showHide === false ? (
+            <div>
+              <button
+                className="exppense-open-form input"
+                type="submit"
+                onClick={showHideForm}
+              >
+                add new expense
+              </button>
+            </div>
+          ) : (
+            <div>
+              <label>Title</label>
+              <input
+                className="text-input  input"
+                type="text"
+                placeholder="Enter Title "
+                value={enteredTitle}
+                onChange={titleChangeHandler}
+              ></input>
+              <label>Amount</label>
+              <input
+                className="number-input  input"
+                type="number"
+                min="1.00"
+                step="1.00"
+                placeholder="Enter Amount"
+                value={enteredAmount}
+                onChange={amountChangeHandler}
+              ></input>
+              <label>Date</label>
+              <input
+                className="date-input  input"
+                type="date"
+                value={enteredDate}
+                onChange={dateChangeHandler}
+              ></input>
+              <br />
+              <button className="input" onClick={showHideForm}>
+                cencel
+              </button>
+              <button className="text-input  input" type="submit">
+                add expense
+              </button>
+            </div>
+          )}
+        </form>
+      </div>
+      <div className="chart-main">chart div</div>
+      <div className="filter-main">filter year div</div>
+      <div className="expense-list-main">expense list div</div>
+    </div>
+  );
 };
 
 export default Expenses;
